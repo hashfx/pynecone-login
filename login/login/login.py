f908c4c8bc2e8f550ad0d070ba6bdbd6f60304f9
Harsh Soni <algebra.soni@gmail.com>
2023-03-29 13:41:29 +0000

    UI designed

--- login/login/login.py
+++ login/login/login.py
@@ -1,7 +1,127 @@
 import pynecone as pc
 
-def index() -> pc.Component:
-    pass
+# input field
+
+
+def get_input_field(icon: str, placeholder: str, _type: str):
+    return pc.container(
+        # horizontally stack icon and input field
+        pc.hstack(
+            pc.icon(
+                tag=icon,
+                color="white",
+                fontSize="11px",
+            ),
+            pc.input(
+                placeholder = placeholder,
+                border = "0px",
+                focus_border_color = "None",
+                color = "white",
+                fontWeight = "semibold",
+                fontSize = "11px",
+                type = _type,
+            ),
+        ),
+        # container settings
+        borderBottom = "0.1px solid gray",
+        width = "300px",
+        height = "45px",
+    )
+
+
+def index():
+    main_container = pc.container(
+
+        # vertical stack for input fields and buttons
+        pc.vstack(
+            pc.container(height="65"),
+            pc.container(
+                pc.text(
+                    "Sign In",
+                    fontSize="20px",
+                    color="white",
+                    fontWeight="bold",
+                    letterSpacing="2px",
+                ),
+                # text settings
+                width="250px",
+                center_content=True
+            ),
+
+            pc.container(
+                pc.text(
+                    "Made with 🤍 using Pynecone",
+                    fontSize="12px",
+                    color="white",
+                    fontWeight="#eeeeee",
+                    letterSpacing="0.25px",
+                ),
+                # text settings
+                width="250px",
+                center_content=True
+            ),
+            # padding
+            pc.container(height="50px"),
+
+            # custom text fields
+            get_input_field("email", "Email", ""),
+            pc.container(height="5px"),
+
+            # password input field
+            get_input_field("lock", "Password", "password"),
+            pc.container(height="5px"),
+
+            # forgot password
+            pc.container(
+                pc.text(
+                    "Forgot Password",
+                    color = "white",
+                    fontSize = "12px",
+                    textAlign = "end",
+                )
+            ),
+            pc.container(height = "55px"),
+
+            # form button
+            pc.container(
+                pc.button(
+                    pc.text(
+                        "Sign In",
+                        color = "white",
+                        weight = "bold",
+                    ),
+                    # button settings
+                    width = "300px",
+                    height = "45px",
+                    color_scheme = "blue"
+                ),
+            ),
+        ),
+
+        # container settings
+        width="400px",
+        height="75vh",
+        center_content=True,
+        bg="1D2330",
+        borderRadius="15px",
+        boxShadow="41px -41px 82px #0d0f15, -41px 41px 82px #2d374b",
+
+    )
+
+    # main stack to return
+    _main = pc.container(
+        main_container,
+        # stack settings
+        center_content=True,
+        justifyContent="center",
+        maxWidth="auto",
+        height='100vh',
+        bg="#1D2330"
+    )
+
+    # return main stack
+    return _main
+
 
 app = pc.App()
 app.add_page(index)
